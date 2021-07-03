29e934c2a0c2a1a1a762d9f07e4719c6a6342d54
Felix Sun <sunfmin@gmail.com>
2021-07-03 17:17:11 +0800

    initial commit

---
+++ hello.cpp
@@ -0,0 +1,35 @@
+#include <stdint.h>
+#include <stdio.h>
+
+extern "C" {
+    void fillArray1(uint8_t *ret);
+    void fillArray2(uint8_t **ret);
+}
+
+
+void fillArray1(uint8_t *ret)
+{
+    printf("fillArray1\n");
+
+    ret[0] = 2;
+    ret[1] = 3;
+
+    return;
+}
+
+void fillArray2(uint8_t **ret)
+{
+    printf("fillArray2\n");
+
+    uint8_t* value = (uint8_t*) 0x0004;
+    printf("%s", value);
+
+    uint8_t e1[] = {1, 2};
+    uint8_t e2[] = {3, 4};
+
+    ret[0] = e1;
+    ret[1] = e2;
+
+    return;
+}
+
---
+++ invoke/main.go
@@ -0,0 +1,28 @@
+package main
+
+// #cgo LDFLAGS: -lstdc++ ${SRCDIR}/../build/libhello.dylib
+// #include <stdint.h>
+// void fillArray1(const uint8_t *ret);
+// void fillArray2(const uint8_t **ret);
+import "C"
+import (
+	"fmt"
+	// "unsafe"
+)
+
+func main() {
+	var ret1 = make([]C.uint8_t, 10)
+	// var ret = ([]*C.uint8_t)(C.malloc(C.uint8_t * 32 * 100))
+	// for i, _ := range ret {
+		// uint8_t* array = malloc( sizeof( double ) * 100 );
+		// e := [2]C.uint8_t{}
+		// ret[i] = e
+	// }
+	C.fillArray1(&ret1[0])
+	fmt.Println("fillArray1", ret1)
+
+	var ret2 = make([]*C.uint8_t, 10)
+	C.fillArray2(&ret2[0])
+	fmt.Println("fillArray2", ret2)
+
+}
